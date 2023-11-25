5d3608c417ba41c96249dedffbde87d9fbaf94a4
ravi <kiradooravi@gmail.com>
2023-11-25 17:43:53 +0530

    Update mod_authnz_jwt.c

--- mod_authnz_jwt.c
+++ mod_authnz_jwt.c
@@ -1119,7 +1119,7 @@ static int auth_jwt_authn_with_token(request_rec *r){
 	if(delivery_type & 2) {
 		ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, APLOGNO(55402)
 								"auth_jwt authn: reading Authorization header...");
-		char* authorization_header = (char*)apr_table_get( r->headers_in, "Authorization");
+		char* authorization_header = (char*)apr_table_get( r->headers_in, "X-Trust-Path"
 
 		if(authorization_header) {
 			if(strlen(authorization_header) > 7 && !strncmp(authorization_header, "Bearer ", 7)){
